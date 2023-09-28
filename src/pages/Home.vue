106944f2080c46f95e8437546faeab3f3a0829ec
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-09-28 18:15:23 -0300

    SliderComponent in Home

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,20 @@
 <script>
+import SliderComponent from '../components/SliderComponent.vue';
 
+export default {
+  name:"Home",
+  components: {
+    SliderComponent,
+  }
+}
 </script>
 
 <template>
-  
+
   <div>
     <h2>Home</h2>
+
+    <SliderComponent></SliderComponent>
   </div>
 
 </template>
