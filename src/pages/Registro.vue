00aa81f0f59dcf098a7e0d74c2430f9f1b76c6a2
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-10-15 20:55:34 -0300

    proceso de register

--- src/pages/Registro.vue
+++ src/pages/Registro.vue
@@ -3,7 +3,7 @@ import BaseH2 from '../components/BaseH2.vue';
 import BaseButton from '../components/BaseButton.vue';
 import BaseInput from '../components/BaseInput.vue';
 import BaseLabel from '../components/BaseLabel.vue';
-
+import {register} from '../services/auth'
 export default {
   name:"Registro",
   components: {
@@ -11,7 +11,27 @@ export default {
     BaseButton,
     BaseInput,
     BaseLabel,
-  }
+  },
+  data(){
+    return{
+      loadingRegistro: false,
+      nuevoUsuario: {
+        email:'',
+        password:'',
+      },
+    }
+  },
+  methods:{
+    async registrarUsuario(){
+      this.loadingRegistro = true
+      try {
+        await register({...this.nuevoUsuario})
+      } catch (error) {
+        console.log(error)
+      }
+      this.loadingRegistro = false
+    }
+  },
 }
 </script>
 
@@ -21,15 +41,15 @@ export default {
         <BaseH2>Registro</BaseH2>
       </div>
         <section class="lg:mx-6 lg:p-4 lg:px-[20%]">
-            <form action="" autocomplete="off" method="POST"
+            <form action="" autocomplete="off" method="POST" @submit.prevent="registrarUsuario"
                 class="max-w-[100%] block rounded-lg bg-white lg:p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
                 <div class="relative mb-6" data-te-input-wrapper-init>
-                    <BaseInput type="text" name="user_name"/>
+                    <BaseInput type="text" name="user_name" />
                     <BaseLabel for="user_name">Nombre de usuario</BaseLabel>
                 </div>
 
                 <div class="relative mb-6" data-te-input-wrapper-init>
-                    <BaseInput type="email" name="user_email" />
+                    <BaseInput type="email" name="user_email" v-model="nuevoUsuario.email"/>
                     <BaseLabel for="user_email">Email</BaseLabel>
                 </div>
 
@@ -39,7 +59,7 @@ export default {
                 </div>
 
                 <div class="relative mb-6" data-te-input-wrapper-init>
-                    <BaseInput type="password" name="password" />
+                    <BaseInput type="password" name="password" v-model="nuevoUsuario.password"/>
                     <BaseLabel for="password">Contraseña</BaseLabel>
                 </div>
 
@@ -48,7 +68,7 @@ export default {
                     <BaseLabel for="password_repeat">Repetir contraseña</BaseLabel>
                 </div>
 
-                <BaseButton>Ingresar</BaseButton>
+                <BaseButton :loading="loadingRegistro">Ingresar</BaseButton>
             </form>
         </section>
     </div>
