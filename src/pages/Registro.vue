153b28d2ef1bf90d03f6ba7ce9760d2396837575
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-10-15 20:57:32 -0300

    saco campos innecesarios

--- src/pages/Registro.vue
+++ src/pages/Registro.vue
@@ -43,30 +43,30 @@ export default {
         <section class="lg:mx-6 lg:p-4 lg:px-[20%]">
             <form action="" autocomplete="off" method="POST" @submit.prevent="registrarUsuario"
                 class="max-w-[100%] block rounded-lg bg-white lg:p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-                <div class="relative mb-6" data-te-input-wrapper-init>
+                <!-- <div class="relative mb-6" data-te-input-wrapper-init>
                     <BaseInput type="text" name="user_name" />
                     <BaseLabel for="user_name">Nombre de usuario</BaseLabel>
-                </div>
+                </div> -->
 
                 <div class="relative mb-6" data-te-input-wrapper-init>
                     <BaseInput type="email" name="user_email" v-model="nuevoUsuario.email"/>
                     <BaseLabel for="user_email">Email</BaseLabel>
                 </div>
 
-                <div class="relative mb-6" data-te-input-wrapper-init>
+                <!-- <div class="relative mb-6" data-te-input-wrapper-init>
                     <BaseInput type="email" name="user_email_again"/>
                     <BaseLabel for="user_email_again">Repetir email</BaseLabel>
-                </div>
+                </div> -->
 
                 <div class="relative mb-6" data-te-input-wrapper-init>
                     <BaseInput type="password" name="password" v-model="nuevoUsuario.password"/>
                     <BaseLabel for="password">Contraseña</BaseLabel>
                 </div>
 
-                <div class="relative mb-6" data-te-input-wrapper-init>
+                <!-- <div class="relative mb-6" data-te-input-wrapper-init>
                     <BaseInput type="password" name="password_repeat"/>
                     <BaseLabel for="password_repeat">Repetir contraseña</BaseLabel>
-                </div>
+                </div> -->
 
                 <BaseButton :loading="loadingRegistro">Ingresar</BaseButton>
             </form>
