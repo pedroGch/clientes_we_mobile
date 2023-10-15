70e8509b7a87806e6d0c40d5f27286f3fa5e5d16
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-10-15 20:54:58 -0300

    estilo disabled en base input

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -8,7 +8,7 @@ export default {
 
 <template>
   <input
-    class="peer block min-h-[auto] w-full rounded border bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+    class="peer block min-h-[auto] w-full rounded border bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0 disabled:bg-gray-200"
     :value="modelValue" @input="$emit('update:modelValue', $event.target.value)">
 </template>
 
