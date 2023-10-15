a2e901e29f239ed9b156472d007468ef166b4d47
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-10-15 17:11:05 -0300

    cambio en la llamada a la configuracion

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -5,7 +5,7 @@ import {APIKEY,
   APPID,
   MESSAGINGSENDERID,
   MASUREMENTID,
-} from '../../config'
+} from '../../conf'
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 import { getAuth } from "firebase/auth"
