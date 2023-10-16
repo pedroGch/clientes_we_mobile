18f6efe80a74774259da1e5110ff12acb1f27b06
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-10-15 23:39:03 -0300

    agrego vista perfil de usuario

---
+++ src/pages/PerfilUsuario.vue
@@ -0,0 +1,26 @@
+<script>
+
+
+export default {
+  setup() {
+    return{
+      usuario:{
+        id: '',
+        email:''
+      }
+    }
+  },
+  mounted(){
+
+  },
+  methods:{
+
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>perfil de usuario</h1>
+  </div>
+</template>
