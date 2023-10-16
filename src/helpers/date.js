56bd465ec565f8d4592eb4034d846dd8ec7b06a9
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-10-16 05:03:47 -0300

    arreglo date para que pueda devolver null

--- src/helpers/date.js
+++ src/helpers/date.js
@@ -1,9 +1,10 @@
 /**
  *
- * @param {Date} date
- * @returns
+ * @param {Date|null} date
+ * @returns {String|null}
  */
 export function dateToString(date){
+  if (date == null) return null
   return new Intl.DateTimeFormat('es-AR',{
     year: 'numeric',
     month: '2-digit',
