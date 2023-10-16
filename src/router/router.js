ad94fb5f3d02e30a67334d113eacbbf242f2e937
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-10-15 23:38:42 -0300

    agrego ruta de perfil de usiaro

--- src/router/router.js
+++ src/router/router.js
@@ -6,6 +6,7 @@ import Chat from '../pages/Chat.vue';
 import Registro from '../pages/registro.vue';
 import IniciarSesion from '../pages/IniciarSesion.vue';
 import PerfilAdmin from '../pages/PerfilAdmin.vue';
+import PerfilUsuario from '../pages/PerfilUsuario.vue';
 import { createRouter, createWebHashHistory } from 'vue-router'; // Importamos el router de Vue
 import { subscribeToAuth } from '../services/auth';
 
@@ -16,12 +17,13 @@ import { subscribeToAuth } from '../services/auth';
 // Primero definimos la lista de rutas
 
 const routes = [
-  {path: '/',               component: Home},
-  {path: '/cursos',         component: Cursos},
-  {path: '/chat',           component: Chat, meta:{requiresAuth:true}},
-  {path: '/registro',       component: Registro},
-  {path: '/iniciar-sesion', component: IniciarSesion},
-  {path: '/perfil-admin',   component: PerfilAdmin, meta:{requiresAuth:true}},
+  {path: '/',                     component: Home},
+  {path: '/cursos',               component: Cursos},
+  {path: '/chat',                 component: Chat, meta:{requiresAuth:true}},
+  {path: '/registro',             component: Registro},
+  {path: '/iniciar-sesion',       component: IniciarSesion},
+  {path: '/perfil-admin',         component: PerfilAdmin, meta:{requiresAuth:true}},
+  {path: '/perfil-usuario/:id',   component: PerfilUsuario, meta:{requiresAuth:true}},
 ];
 
 
