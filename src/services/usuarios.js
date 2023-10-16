1c243b84db435128b9130e0a152b3743cf1445d1
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-10-16 05:04:04 -0300

    creo usuario.js

---
+++ src/services/usuarios.js
@@ -0,0 +1,28 @@
+import { db } from "./firebase";
+import { doc, getDoc, setDoc, serverTimestamp } from "firebase/firestore";
+
+/**
+ *
+ * @param {string} id
+ * @return {Promise<{id: string, email:string}>}
+ */
+export async function obtenerUsuarioPorId(id) {
+  const refUser = doc(db, `usuarios/${id}`);
+  const docSnapshot = await getDoc(refUser);
+
+  return {
+      id: docSnapshot.id,
+      email: docSnapshot.data().email,
+  }
+}
+
+/**
+ *
+ * @param {string} id
+ * @param {{email: string}} data
+ * @return {Promise}
+ */
+export async function crearPerfilDeUsuario(id, data) {
+  const refUser = doc(db, `usuarios/${id}`);
+  return setDoc(refUser, {...data, created_at: serverTimestamp()});
+}
