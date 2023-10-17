69c98951eec3011e5bac4cd2cfd34f747158767a
Ezequiel <ezequiel.valdiviezo@davinci.edu.ar>
2023-10-17 13:55:05 -0300

    Arme el diseño del chat privado

--- src/pages/ChatPrivado.vue
+++ src/pages/ChatPrivado.vue
@@ -73,38 +73,51 @@ export default{
     </template>
     <template v-else>
 
+          <div class="pb-3">
+            <p class="text-2xl font-bold text-center">Chatea con {{usuario.email}}</p>
+          </div>
+
           <div class="modal-container w-[100%] h-[100%]  mx-auto overflow-y-auto">
               <!-- Contenido del modal -->
               <div class="modal-content py-4 text-left px-6">
-                  <div class="flex justify-between items-center pb-3">
+                  <!-- <div class="flex justify-between items-center pb-3">
                       <p class="text-2xl font-bold">Chatea con {{usuario.email}}</p>
-                  </div>
+                  </div> -->
                   <div class="h-[400px]">
                     <div class="h-[80%]">
 
                       <div v-for="mensaje in mensajes" :key="mensaje.id"
                         :class="
                           {
-                            'bg-gray-100': mensaje.senderId !== authUsuario.id,
-                            'bg-green-200': mensaje.senderId === authUsuario.id,
+                            'bg-gray-100 w-6/12 rounded-lg': mensaje.senderId !== authUsuario.id,
+                            'bg-purple-200 text-right w-6/12 ml-[50%] rounded-lg': mensaje.senderId === authUsuario.id,
                           }"
                       >
-                        <div class="p-2 my-3">
+                        <div class="p-2 my-3 m-5">
                           {{mensaje.message}}
                         </div>
-                        <div class="text-right">{{formatoDia(mensaje.created_at || 'enviado...')}}</div>
-                      </div>
+                        <div class="text-right mr-2 mb-2">{{formatoDia(mensaje.created_at || 'enviado...')}}</div>
+
+                    </div>
                     </div>
 
                   </div>
               </div>
-              <div class="modal-footer px-6 w-[100%]">
+              <!-- <div class="modal-footer px-6 w-[100%]">
                 <form action="" class="flex justify-between" @submit.prevent="manejadorEnvioMensaje">
                   <BaseInput type="text" placeholder="Ingresá tu mensaje" id="message" name="message" v-model="nuevoMensaje.mensaje"/>
                   <BaseButton ></BaseButton>
                 </form>
-              </div>
+              </div> -->
           </div>
+
+          <div class="modal-footer px-6 w-[100%] my-6">
+                <form action="" class="flex justify-between" @submit.prevent="manejadorEnvioMensaje">
+                  <BaseInput class="m-2 border-2 border-indigo-900" type="text" placeholder="Ingresá tu mensaje" id="message" name="message" v-model="nuevoMensaje.mensaje"/>
+                  <BaseButton class="m-2"></BaseButton>
+                </form>
+          </div>
+
     </template>
   </div>
 </template>
