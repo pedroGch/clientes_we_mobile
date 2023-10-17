64c9e4e49a0ae38e3e8e848706d2deb45e127ec3
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-10-17 14:44:36 -0300

    loader en vista cursos

--- src/pages/Cursos.vue
+++ src/pages/Cursos.vue
@@ -2,15 +2,18 @@
 import BaseH2 from '../components/BaseH2.vue';
 import BaseButton from '../components/BaseButton.vue';
 import { cargarProductos } from '../services/productos';
+import Loader from '../components/Loader.vue';
 
 export default {
   name: "Cursos",
   components: {
     BaseH2,
     BaseButton,
+    Loader,
   },
   data() {
     return {
+      isLoading: true,
       productos: []
     };
   },
@@ -18,6 +21,7 @@ export default {
     // Llama a la función cargarProductos para obtener el array de productos
     cargarProductos(productos => {
       this.productos = productos;
+      this.isLoading = false;
     });
   }
 };
@@ -31,99 +35,40 @@ export default {
       <BaseH2>Cursos</BaseH2>
     </div>
 
-
-    <div class="mx-auto max-w-screen-xl">
-
-      <div class="flex flex-wrap justify-around">
-        <div>
-          <div class="max-w-sm rounded overflow-hidden shadow-lg" v-for="producto in productos" :key="producto.id">
-            <img class="w-full" src="../../public/img/curso1.jpg" alt="Sunset in the mountains">
-            <div class="px-6 py-4">
-              <div class="font-bold text-xl mb-2">Curso: "{{ producto.nombre }}"</div>
-              <p class="text-gray-700 text-base">
-                Precio: ${{ producto.precio }}
-              </p>
-              <p class="text-gray-700 text-base">
-                Stock disponible: {{ producto.cupo }} plazas
-              </p>
-              <BaseButton>
-                <router-link :to="'/detalles-curso/' + producto.id">Ver más</router-link>
-              </BaseButton>
+    <template v-if="!isLoading">
+      <div class="mx-auto max-w-screen-xl">
+        <div class="flex flex-wrap justify-around">
+          <div>
+            <div class="max-w-sm rounded overflow-hidden shadow-lg" v-for="producto in productos" :key="producto.id">
+              <img class="w-full" src="../../public/img/curso1.jpg" alt="Sunset in the mountains">
+              <div class="px-6 py-4">
+                <div class="font-bold text-xl mb-2">Curso: "{{ producto.nombre }}"</div>
+                <p class="text-gray-700 text-base">
+                  Precio: ${{ producto.precio }}
+                </p>
+                <p class="text-gray-700 text-base">
+                  Stock disponible: {{ producto.cupo }} plazas
+                </p>
+                <BaseButton>
+                  <router-link :to="'/detalles-curso/' + producto.id">Ver más</router-link>
+                </BaseButton>
+              </div>
             </div>
           </div>
-        </div>
-        <!-- <div class="max-w-sm rounded overflow-hidden shadow-lg">
-      <img class="w-full" src="../../public/img/curso1.jpg" alt="Sunset in the mountains">
-      <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2">Curso 1: "Introducción al Arte de los Mandalas"</div>
-          <p class="text-gray-700 text-base">
-            Precio: $49.99
-          </p>
-          <p class="text-gray-700 text-base">
-            Stock disponible: 50 plazas
-          </p>
-          <BaseButton>
-              <router-link to="/detalles-curso">Ver más</router-link>
-            </BaseButton>
-        </div>
-    </div> -->
 
-        <!-- <div class="max-w-sm rounded overflow-hidden shadow-lg">
-      <img class="w-full" src="../../public/img/curso2.jpg" alt="Sunset in the mountains">
-      <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2">Curso 2: "Domina el Arte del Dibujo Zentangle"</div>
-          <p class="text-gray-700 text-base">
-            Precio: $59.99
-          </p>
-          <p class="text-gray-700 text-base">
-            Stock disponible: 40 plazas
-          </p>
-          <BaseButton>
-              <router-link to="/detalles-curso">Ver más</router-link>
-            </BaseButton>
         </div>
-    </div> -->
-
-        <!-- <div class="max-w-sm rounded overflow-hidden shadow-lg">
-      <img class="w-full" src="../../public/img/curso3.jpg" alt="Sunset in the mountains">
-      <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2">Curso 3: "Mandalas Terapéuticos: Sanando con el Arte"</div>
-          <p class="text-gray-700 text-base">
-            Precio: $69.99
-          </p>
-          <p class="text-gray-700 text-base">
-            Stock disponible: 30 plazas
-          </p>
-          <BaseButton>
-              <router-link to="/detalles-curso">Ver más</router-link>
-            </BaseButton>
-        </div>
-    </div> -->
-
-        <!-- <div class="max-w-sm rounded overflow-hidden shadow-lg">
-      <img class="w-full" src="../../public/img/curso4.jpg" alt="Sunset in the mountains">
-      <div class="px-6 py-4">
-          <div class="font-bold text-xl mb-2">Curso 4: "Suscripción Mensual: Meditaciones Guiadas y Bienestar"</div>
-          <p class="text-gray-700 text-base">
-            Precio: $19.99
-          </p>
-          <p class="text-gray-700 text-base">
-            Stock disponible: Ilimitado
-          </p>
-          <BaseButton>
-              <router-link to="/detalles-curso">Ver más</router-link>
-            </BaseButton>
-        </div>
-    </div> -->
       </div>
+    </template>
+    <template class="flex justify-center" v-else>
+      <Loader class="" />
+    </template>
 
-
-      <div class="mt-16 text-center font-tipografiaTitulares font-bold text-2xl">
-        <p>Recuerda que nuestros cursos están diseñados para enriquecer tu vida, ayudándote a explorar tu creatividad,
-          encontrar la serenidad y conectarte con tu fluidez interior. ¡No dudes en inscribirte en el curso que más te
-          llame la atención y comenzar tu viaje con Dhara!</p>
-      </div>
-
+    <div class="mt-16 text-center font-tipografiaTitulares font-bold text-2xl">
+      <p>Recuerda que nuestros cursos están diseñados para enriquecer tu vida, ayudándote a explorar tu creatividad,
+        encontrar la serenidad y conectarte con tu fluidez interior. ¡No dudes en inscribirte en el curso que más te
+        llame la atención y comenzar tu viaje con Dhara!</p>
     </div>
+
   </div>
+
 </template>
