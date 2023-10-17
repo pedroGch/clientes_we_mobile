f652d7cf8db42ae9574189caea5be938afb2bedf
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-10-17 15:39:45 -0300

    validacion de form registro

--- src/pages/Registro.vue
+++ src/pages/Registro.vue
@@ -4,6 +4,7 @@ import BaseButton from '../components/BaseButton.vue';
 import BaseInput from '../components/BaseInput.vue';
 import BaseLabel from '../components/BaseLabel.vue';
 import {register} from '../services/auth'
+
 export default {
   name:"Registro",
   components: {
@@ -19,6 +20,10 @@ export default {
         email:'',
         password:'',
       },
+      errors: {
+        email: '',
+        password: '',
+      }
     }
   },
   methods:{
@@ -31,7 +36,26 @@ export default {
         console.log(error)
       }
       this.loadingRegistro = false
-    }
+    },
+    validateForm() {
+      this.errors = {};
+
+      if (!this.nuevoUsuario.email) {
+        this.errors.email = 'El email es requerido.';
+      }
+
+      if (!this.nuevoUsuario.password) {
+        this.errors.password = 'La contraseña es requerida.';
+      }
+
+      return Object.keys(this.errors).length === 0;
+    },
+    submitForm() {
+      if (this.validateForm()) {
+
+        this.registrarUsuario();
+      }
+    },
   },
 }
 </script>
@@ -42,32 +66,20 @@ export default {
         <BaseH2>Registro</BaseH2>
       </div>
         <section class="lg:mx-6 lg:p-4 lg:px-[20%]">
-            <form action="" autocomplete="off" method="POST" @submit.prevent="registrarUsuario"
+            <form action="" autocomplete="off" method="POST" @submit.prevent="submitForm"
                 class="max-w-[100%] block rounded-lg bg-white lg:p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-                <!-- <div class="relative mb-6" data-te-input-wrapper-init>
-                    <BaseInput type="text" name="user_name" />
-                    <BaseLabel for="user_name">Nombre de usuario</BaseLabel>
-                </div> -->
 
                 <div class="relative mb-6" data-te-input-wrapper-init>
                     <BaseInput type="email" :disabled="loadingRegistro" name="user_email" v-model="nuevoUsuario.email"/>
                     <BaseLabel for="user_email">Email</BaseLabel>
+                    <p v-if="errors.email" class="error-message">{{ errors.email }}</p>
                 </div>
 
-                <!-- <div class="relative mb-6" data-te-input-wrapper-init>
-                    <BaseInput type="email" name="user_email_again"/>
-                    <BaseLabel for="user_email_again">Repetir email</BaseLabel>
-                </div> -->
-
-                <div class="relative mb-6" data-te-input-wrapper-init>
+               <div class="relative mb-6" data-te-input-wrapper-init>
                     <BaseInput type="password" :disabled="loadingRegistro" name="password" v-model="nuevoUsuario.password"/>
                     <BaseLabel for="password">Contraseña</BaseLabel>
+                    <p v-if="errors.password" class="error-message">{{ errors.password }}</p>
                 </div>
-
-                <!-- <div class="relative mb-6" data-te-input-wrapper-init>
-                    <BaseInput type="password" name="password_repeat"/>
-                    <BaseLabel for="password_repeat">Repetir contraseña</BaseLabel>
-                </div> -->
 
                 <BaseButton :loading="loadingRegistro">Ingresar</BaseButton>
             </form>
