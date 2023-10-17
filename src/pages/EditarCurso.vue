b17192cab92675ef0ab61c9a5a541378ebf8a54e
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-10-17 13:03:42 -0300

    componente EditarCurso + router

---
+++ src/pages/EditarCurso.vue
@@ -0,0 +1,59 @@
+<script>
+import BaseH2 from '../components/BaseH2.vue';
+import BaseButton from '../components/BaseButton.vue';
+import BaseLabel from '../components/BaseLabel.vue';
+import BaseInput from '../components/BaseInput.vue';
+
+export default {
+  name: "EditarCurso",
+  components: {
+    BaseH2,
+    BaseButton,
+    BaseLabel,
+    BaseInput
+},
+data() {
+  return {
+    cargando: true,
+    curso: {
+      id: '',
+      titulo: '',
+      descripcion: '',
+      precio: '',
+      cupo: ''
+    }
+  }
+},
+}
+</script>
+
+<template>
+  <div class="">
+
+    <div>
+      <div class="text-center mt-16 mb-16">
+        <BaseH2>Editar información del curso</BaseH2>
+      </div>
+      <div >
+        <form action="#" method="POST" id="form-edit" @submit.prevent="">
+          <BaseLabel for="titulo"></BaseLabel>
+          <BaseInput type="text" name="titulo" id="titulo" placeholder="Título" v-model="curso.titulo" required />
+
+          <BaseLabel for="descripcion"></BaseLabel>
+          <BaseInput type="text" name="descripcion" id="descripcion" v-model="curso.descripcion" required />
+
+          <BaseLabel for="precio"></BaseLabel>
+          <BaseInput type="number" name="precio" id="precio" v-model="curso.precio" required />
+
+          <BaseLabel for="cupo"></BaseLabel>
+          <BaseInput type="number" name="cupo" id="cupo" v-model="curso.cupo" required />
+
+          <BaseButton type="submit">Editar</BaseButton>
+        </form>
+      </div>
+
+    </div>
+  </div>
+
+
+</template>
--- src/router/router.js
+++ src/router/router.js
@@ -9,6 +9,7 @@ import IniciarSesion from '../pages/IniciarSesion.vue';
 import PerfilAdmin from '../pages/PerfilAdmin.vue';
 import PerfilUsuario from '../pages/PerfilUsuario.vue';
 import ChatPrivado from '../pages/ChatPrivado.vue';
+import EditarCurso from '../pages/EditarCurso.vue';
 import { createRouter, createWebHashHistory } from 'vue-router'; // Importamos el router de Vue
 import { subscribeToAuth } from '../services/auth';
 
@@ -21,13 +22,14 @@ import { subscribeToAuth } from '../services/auth';
 const routes = [
   {path: '/',                     component: Home},
   {path: '/cursos',               component: Cursos},
-  {path: '/detalles-curso',        component: DetalleCurso},
+  {path: '/detalles-curso/:id',   component: DetalleCurso},
   {path: '/chat',                 component: Chat, meta:{requiresAuth:true}},
   {path: '/registro',             component: Registro},
   {path: '/iniciar-sesion',       component: IniciarSesion},
   {path: '/perfil-admin',         component: PerfilAdmin, meta:{requiresAuth:true}},
   {path: '/perfil-usuario/:id',   component: PerfilUsuario, meta:{requiresAuth:true}},
   {path: '/usuario/:id/chat',     component: ChatPrivado, meta:{requiresAuth:true}},
+  {path: '/editar-curso/:id',     component: EditarCurso, meta:{requiresAuth:true}},
 ];
 
 
