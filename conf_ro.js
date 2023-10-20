7814d6033bec94165e89b5fc73e3d750073452d4
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-10-20 14:51:48 -0300

    borrado conf_ro y conf_valdi

--- conf_ro.js
+++
@@ -1,8 +0,0 @@
-export const APIKEY="AIzaSyD0gxCvDjSluyWrNM-A8sv9NwmOhaEDOg4"
-export const AUTHDOMAIN= "clientes-web-2023-davinci.firebaseapp.com"
-export const PROJECTID="clientes-web-2023-davinci"
-export const STORAGEBUCKET="clientes-web-2023-davinci.appspot.com"
-export const MESSAGINGSENDERID="384231176118"
-export const APPID="1:384231176118:web:ab1d64647a3e8e70a1b317"
-export const MASUREMENTID="G-Q2KPLDFQS9"
-export const BDNAME="chats"
